8b298f5b5264ab1187701a0a573e433482b68b4c
guandoudou <guandoudou@guandoudous-MacBook-Air.local>
2017-11-21 14:49:15 +0800

    Initial Commit

---
+++ MyGesture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGesture
+//
+//  Created by guandoudou on 2017/11/21.
+//  Copyright © 2017年 京. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
